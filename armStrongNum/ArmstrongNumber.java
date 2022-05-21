c6a55e352b202e484e1c3fb781f8738665b38ddd
princiladaniel <93445762+princiladaniel@users.noreply.github.com>
2022-05-21 16:20:14 +0530

    Add files via upload

---
+++ armStrongNum/ArmstrongNumber.java
@@ -0,0 +1,28 @@
+package armStrongNum;
+
+import java.util.Arrays;
+
+public class ArmstrongNumber {
+	public static boolean armNum(long n) {
+		boolean flag =true;
+		long calculated=0;long reminder;  
+		long original=n;  
+		while(n>0){  
+			reminder=n%10;  
+			n=n/10;  
+			calculated=calculated+(reminder*reminder*reminder);  
+		} 
+		
+		if(original!=calculated)  {
+			
+			flag=false;
+		}
+		else {
+			flag=true;
+		}
+		return flag;   
+}
+
+}
+
+
